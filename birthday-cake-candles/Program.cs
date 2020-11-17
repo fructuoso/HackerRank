d999f2043f5495ac3c670c3dd2bca51134ce76d1
Fructuoso <victor@fructuoso.com.br>
2020-11-17 11:58:03 -0300

    Resolvendo 'birthday-cake-candles' e 'time-conversion'

---
+++ birthday-cake-candles/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HackerRank
+{
+    public static class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            int result = birthdayCakeCandles(candles);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        private static int birthdayCakeCandles(List<int> candles)
+        {
+            var tallestCount = 0;
+            var tallestValue = 0;
+
+            for (int x = 0; x < candles.Count; x++)
+            {
+                if (candles[x] > tallestValue)
+                {
+                    tallestValue = candles[x];
+                    tallestCount = 1;
+                }
+                else if (candles[x] == tallestValue) tallestCount++;
+            }
+
+            return tallestCount;
+        }
+    }
+}
---
+++ time-conversion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HackerRank
+{
+    public static class Solution
+    {
+        public static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+
+            string result = timeConversion(s);
+            
+            TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            tw.WriteLine(result);
+
+            tw.Flush();
+            tw.Close();
+        }
+
+        private static string timeConversion(string s)
+        {
+            int hour = Convert.ToInt32(s.Substring(0, 2));
+            int minute = Convert.ToInt32(s.Substring(3, 2));
+            int second = Convert.ToInt32(s.Substring(6, 2));
+
+            string period = s.Substring(8, 2);
+
+            if (period == "PM" && hour < 12) hour += 12;
+            if (period == "AM" && hour >= 12) hour -= 12;
+
+            return $"{hour:00}:{minute:00}:{second:00}";
+        }
+    }
+}
