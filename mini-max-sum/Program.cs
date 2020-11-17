8caf98728f99387dffac85acca6b247e3eaefe80
Fructuoso <victor@fructuoso.com.br>
2020-11-17 11:27:38 -0300

    Resolvendo 'mini-max-sum'

---
+++ mini-max-sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HackerRank
+{
+    public static class Solution
+    {
+        // Complete the miniMaxSum function below.
+        static void miniMaxSum(int[] arr)
+        {
+            int min = arr[0];
+            int max = arr[0];
+            long sum = 0;
+
+            for (int x = 0; x < arr.Length; x++)
+            {
+                if (arr[x] < min) min = arr[x];
+                if (arr[x] > max) max = arr[x];
+                sum += arr[x];
+            }
+
+            Console.WriteLine($"{sum - max} {sum - min}");
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            miniMaxSum(arr);
+        }
+    }
+}
