43b8f1bf1fd23fae05d17ad22fadf836c2e80c4e
Fructuoso <victor@fructuoso.com.br>
2020-11-20 11:46:40 -0300

    Resolvendo 'staircase'

---
+++ staircase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public static class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string('#', i).PadLeft(n));
+            }
+        }
+    }
+}
