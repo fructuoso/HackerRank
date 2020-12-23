357697eab8f21931b61ab4f8da127150d46d846e
Fructuoso <victor@fructuoso.com.br>
2020-12-23 13:49:38 -0300

    Resolvendo desafios da certificação

---
+++ employeesmanagement/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution
+{
+    public class Solution
+    {
+
+        public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+        {
+            return employees.GroupBy(e => e.Company).OrderBy(g => g.Key).ToDictionary(g => g.Key, g => Convert.ToInt32(Math.Round(g.Average(e => e.Age), 0)));
+        }
+        
+        public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+        {
+            return employees.GroupBy(e => e.Company).OrderBy(g => g.Key).ToDictionary(g => g.Key, g => g.Count());
+        }
+        
+        public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+        {
+            return employees.GroupBy(e => e.Company).OrderBy(g => g.Key).ToDictionary(g => g.Key, g => g.OrderByDescending(e => e.Age).FirstOrDefault());
+        }
+
+        public static void Main()
+        {   
+            int countOfEmployees = int.Parse(Console.ReadLine());
+            
+            var employees = new List<Employee>();
+            
+            for (int i = 0; i < countOfEmployees; i++)
+            {
+                string str = Console.ReadLine();
+                string[] strArr = str.Split(' ');
+                employees.Add(new Employee { 
+                    FirstName = strArr[0], 
+                    LastName = strArr[1], 
+                    Company = strArr[2], 
+                    Age = int.Parse(strArr[3]) 
+                    });
+            }
+            
+            foreach (var emp in AverageAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+            }
+            
+            foreach (var emp in CountOfEmployeesForEachCompany(employees))
+            {
+                Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+            }
+            
+            foreach (var emp in OldestAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+            }
+        }
+    }
+    
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+    }
+}   
---
+++ notesstore/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution
+{
+    public class Note
+    {
+        public string State { get; set; }
+        public string Name { get; set; }
+
+        public Note(string state, string name)
+        {
+            State = state;
+            Name = name;
+        }
+    }
+
+    public class NotesStore
+    {
+        private readonly IList<string> _ValidStates = new List<string>() { "completed", "active", "others" };
+        private readonly IList<Note> _Notes = new List<Note>();
+        public NotesStore() { }
+        public void AddNote(String state, String name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new Exception("Name cannot be empty");
+
+            if (!_ValidStates.Contains(state)) throw new Exception($"Invalid state {state}");
+
+            _Notes.Add(new Note(state, name));
+        }
+        public List<String> GetNotes(String state)
+        {
+
+            if (!_ValidStates.Contains(state)) throw new Exception($"Invalid state {state}");
+
+            return _Notes
+                .Where(n => n.State == state)
+                .Select(n => n.Name)
+                .ToList();
+        }
+    }
+
+    public class Solution
+    {
+        public static void Main()
+        {
+            var notesStoreObj = new NotesStore();
+            var n = int.Parse(Console.ReadLine());
+            for (var i = 0; i < n; i++)
+            {
+                var operationInfo = Console.ReadLine().Split(' ');
+                try
+                {
+                    if (operationInfo[0] == "AddNote")
+                        notesStoreObj.AddNote(operationInfo[1], operationInfo.Length == 2 ? "" : operationInfo[2]);
+                    else if (operationInfo[0] == "GetNotes")
+                    {
+                        var result = notesStoreObj.GetNotes(operationInfo[1]);
+                        if (result.Count == 0)
+                            Console.WriteLine("No Notes");
+                        else
+                            Console.WriteLine(string.Join(",", result));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Parameter");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+    }
+}
