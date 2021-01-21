83b9ba85148be6af6b94a35491d8b671147d4a3e
Fructuoso <victor@fructuoso.com.br>
2021-01-21 16:01:51 -0300

    Resolvendo o 'crush'

---
+++ crush/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries)
+    {
+        int[] temp = new int[n];
+        long result = 0;
+
+        for (int q = 0; q < queries.Length; q++)
+        {
+            int[] query = queries[q];
+
+            temp[query[0] - 1] += query[2];
+            
+            if (query[1] < n) temp[query[1]] -= query[2];
+        }
+
+        long current = 0;
+        for (int t = 0; t < temp.Length; t++)
+        {
+            current += temp[t];
+
+            result = Math.Max(result, current);
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nm = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nm[0]);
+
+        int m = Convert.ToInt32(nm[1]);
+
+        int[][] queries = new int[m][];
+
+        for (int i = 0; i < m; i++)
+        {
+            queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
