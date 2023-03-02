53028cfa92acf1a3c3bafa66ec2fbd6c5e5d9e5d
Victor Fructuoso <victor@fructuoso.com>
2023-03-02 11:06:29 -0300

    Resolvendo array-left-rotation

---
+++ array-left-rotation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<int> rotateLeft(int d, List<int> arr)
+    {
+        Queue<int> queue = new Queue<int>(arr);
+
+        for (int i = 0; i < d; i++)
+        {
+            int value = queue.Dequeue();
+            queue.Enqueue(value);
+        }
+
+        return queue.ToList();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.rotateLeft(d, arr);
+
+        
+        Console.WriteLine(String.Join(" ", result));
+        // textWriter.WriteLine(String.Join(" ", result));
+
+        // textWriter.Flush();
+        // textWriter.Close();
+    }
+}
